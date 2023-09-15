94839ec68742ff3dd61262539e21faca8ecacf57
kiraashley24 <kiraselucky@gmail.com>
2023-09-15 12:50:25 -0600

    Added NumPY and Matplotlib. Pulled data and graphed all five stocks. Showed graphs.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # miniproject1KS
 
+INF 601 - Advanced Programming in Python
+Kira Selucky
+Mini Project 1
+
 ## Description
 This project will be using the packages NumPy and Matplotlib in order to create 5 graphs that output as PNG files.
 
---
+++ main.py
@@ -0,0 +1,58 @@
+# INF 601 - Advanced Programming in Python
+# Kira Selucky
+# Mini Project 1
+
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
+#(5/5 points) Initial comments with your name, class and project at the top of your .py file.
+#(5/5 points) Proper import of packages used.
+#(20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
+#(10/10 points) Store this information in a list that you will convert to a array in NumPy.
+#(10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
+#(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
+#(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+#(10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
+#(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
+
+def getClosing(ticker):
+    #Get closing price for last 10 trading days
+    stock = yf.Ticker(ticker)
+    # get historical market data
+    hist = stock.history(period="10d")
+
+    # Create empty closing list for MSFT
+    closingList = []
+
+    # Loop through closing prices and add to list
+    for price in hist["Close"]:
+        closingList.append(price)
+
+    return closingList
+
+stocks = ["MSFT", "AAPL", "GME", "SONY", "META"]
+
+for stock in stocks:
+    stockClosing = np.array(getClosing(stock))
+    days = list(range(1, len(stockClosing)+1))
+
+    #Plot graph
+    plt.plot(days, stockClosing)
+
+    prices = getClosing(stock)
+    prices.sort()
+    low_price = prices[0]
+    high_price = prices[-1]
+
+    # Set x-axis min and max
+    #form [xmin, xmax, ymin, ymax]
+    plt.axis([1,10, low_price-2, high_price+2])
+
+    # Labels for the graph
+    plt.xlabel("Days")
+    plt.ylabel("Closing Price")
+    plt.title("Closing Price for " + stock)
+
+    # Show graph
+    plt.show()
